16781e62b32a7efd94f7877aa1e5dc620eae0827
Alan Raison <alan.raison@newredo.com>
2026-03-17 15:46:11 +0000

    fix: remove trailling period from DNS values

--- cloudflare-dns/main.tf
+++ cloudflare-dns/main.tf
@@ -7,8 +7,8 @@ data "cloudflare_zone" "this" {
 resource "cloudflare_dns_record" "this" {
   for_each = {
     for dvo in var.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
+      name   = trimsuffix(dvo.resource_record_name, ".")
+      record = trimsuffix(dvo.resource_record_value, ".")
       type   = dvo.resource_record_type
     }
   }
